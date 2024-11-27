f1a92d9ff4d0bcc63f4ecffe0fd08c6e5ecd6f42
Chaithanya Shaji <139004346+chaithanyashaji@users.noreply.github.com>
2024-11-28 01:07:36 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: '0.0.0.0', // Allow the app to be accessed from outside the container (important for Render)
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Use the dynamic port provided by Render, fallback to 5173 for local development
   },
 })
